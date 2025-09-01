e170c358c5874ec7d875f37bfaccbdff5781c6d5
EduardoMiyasakii <eduardo.miyasaki@sptech.school>
2025-08-31 21:17:10 -0300

    clonando da branch

--- src/br/com/exemplo/GitUtils.java
+++ src/br/com/exemplo/GitUtils.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class GitUtils {
 
     public static void clonarRepositorio(String urlRepo, File pastaDestino) throws Exception {
-        ProcessBuilder builder = new ProcessBuilder("git", "clone", urlRepo);
+        ProcessBuilder builder = new ProcessBuilder("git", "clone","--branch","feature/ALFANET", urlRepo);
         builder.directory(pastaDestino);
         builder.inheritIO(); // mostra saída no console
         Process process = builder.start();
